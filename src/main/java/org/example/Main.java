ec8303f3d3f13854b60ae79efd8cbf1ae8b2ac50
msalehprofile <mollyhannia@hotmail.co.uk>
2024-05-24 20:43:16 +0100

    amended home

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,28 +13,24 @@ import java.util.List;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static List<Cards> testdeck = new ArrayList<>();
-    static String plone = "molly";
-    static String pltwo = "hpoe";
-
-    public static void adding() {
-        testdeck.add(new Cards(Suit.CLUBS, Rank.TWO));
-        testdeck.add(new Cards(Suit.HEARTS, Rank.TWO));
-        testdeck.add(new Cards(Suit.SPADES, Rank.TWO));
-        testdeck.add(new Cards(Suit.DIAMONDS, Rank.SEVEN));
-        testdeck.add(new Cards(Suit.HEARTS, Rank.EIGHT));
-        testdeck.add(new Cards(Suit.CLUBS, Rank.EIGHT));
-        testdeck.add(new Cards(Suit.SPADES, Rank.EIGHT));
-    };
+    //Below is to be removed, just for here for now for quick testing purposes <3
+//    static List<Cards> testdeck = new ArrayList<>();
+//    static String plone = "molly";
+//    static String pltwo = "hpoe";
+//
+//    public static void adding() {
+//        testdeck.add(new Cards(Suit.CLUBS, Rank.TWO));
+//        testdeck.add(new Cards(Suit.HEARTS, Rank.TWO));
+//        testdeck.add(new Cards(Suit.SPADES, Rank.TWO));
+//        testdeck.add(new Cards(Suit.DIAMONDS, Rank.SEVEN));
+//        testdeck.add(new Cards(Suit.HEARTS, Rank.EIGHT));
+//        testdeck.add(new Cards(Suit.CLUBS, Rank.EIGHT));
+//        testdeck.add(new Cards(Suit.SPADES, Rank.EIGHT));
+//    };
 
     public static void main(String[] args) {
 
-//        adding();
-//        WinConfirmation.checkingNumberOfSuits(testdeck, plone, pltwo);
-
-        WinConfirmation.playerWon(plone, testdeck);
-//        Rummy.playRummy();
-//        HomeScreen.openHome();
+        HomeScreen.openHome();
 
     }
 }
