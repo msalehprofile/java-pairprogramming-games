5417eaa7cc486bc81420c878d5444809d9006fd1
callum <callumgibbons@icloud.com>
2024-05-29 15:29:36 +0100

    Began to implement round interaction that will determine each player's interaction between each card show.

--- src/main/java/org/example/Games/Poker/PlayerInterface.java
+++ src/main/java/org/example/Games/Poker/PlayerInterface.java
@@ -9,10 +9,14 @@ public class PlayerInterface {
     static int numberOfPlayers = getNumberOfPlayers();
     static int activePlayers = numberOfPlayers;
     static int playerRoundSelection;
-    static int playerChips;
+    static int playerChips = 500;
     static int roundChips = 0;
+    static int lastBet = 0;
+    static boolean secondRound = false;
+    static boolean correctInput = false;
     protected static void roundInteraction(int numberOfPlayers){
         for (int i = 0; i < activePlayers; i++) {
+            while (!correctInput){
             Scanner playerSelection = new Scanner(System.in);
             System.out.println("Player " + (i + 1) + " please select what you would like to do");
             System.out.println("1: Fold");
@@ -22,9 +26,12 @@ public class PlayerInterface {
             if (playerRoundSelection == 1) {
                 System.out.println("Player " + (i + 1) + " has folded");
                 activePlayers--;
+                correctInput = true;
             }
             else if (playerRoundSelection == 2){
                 System.out.println("Player" + (i + 1) + "has called");
+
+                correctInput = true;
             }
             else if (playerRoundSelection == 3){
                 System.out.println("How much would you like to raise by?");
@@ -32,13 +39,21 @@ public class PlayerInterface {
                 if (raiseAmount > playerChips) {
                     System.out.println("You do not have enough chips to raise by " + raiseAmount);
                 }
-                else if (raiseAmount < playerChips) {
+                else if (raiseAmount < playerChips && raiseAmount > 0) {
                     System.out.println("Player " + (i + 1) + " has raised by " + raiseAmount);
                     playerChips = playerChips - raiseAmount;
                     roundChips = roundChips + raiseAmount;
-
+                    lastBet = raiseAmount;
+                    correctInput = true;
+                }
+                else if (raiseAmount == playerChips) {
+                    System.out.println("Player " + (i + 1) + " has raised by " + raiseAmount + " and is all in");
+                    playerChips = playerChips - raiseAmount;
+                    roundChips = roundChips + raiseAmount;
+                    lastBet = raiseAmount;
+                    correctInput = true;
                 }
             }
-        }
+        }}
     }
 }
