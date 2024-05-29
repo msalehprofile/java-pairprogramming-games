1a02bf2f48ed29172d8c5ee157e028b96492193c
callum <callumgibbons@icloud.com>
2024-05-29 23:24:28 +0100

    Added the ability to replay with the same players and chips.

--- src/main/java/org/example/Games/Poker/PokerGame.java
+++ src/main/java/org/example/Games/Poker/PokerGame.java
@@ -3,28 +3,43 @@ package org.example.Games.Poker;
 import org.example.SetUp.Cards;
 import org.example.SetUp.AllCards;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import static org.example.Games.Poker.PokerLogic.getNumberOfPlayers;
 import static org.example.Games.Poker.PokerLogic.playerNames;
 
 public class PokerGame {
-    public static void main(String[] args) {
-        PokerLogic pokerLogic = new PokerLogic();
-        pokerLogic.play(); // Initialize the game
-        PlayerInterface.buyIn();
-        pokerLogic.dealCards();
-        PlayerInterface.roundInteraction(getNumberOfPlayers());
-        pokerLogic.dealFlop();
-        PlayerInterface.roundInteraction(getNumberOfPlayers());
-        pokerLogic.dealTurn();
-        PlayerInterface.roundInteraction(getNumberOfPlayers());
-        pokerLogic.dealRiver();
-        PlayerInterface.roundInteraction(getNumberOfPlayers());
+    public static void playPoker() {
+        Scanner scanner = new Scanner(System.in);
 
-        List<Cards> allCards = pokerLogic.getGameCards();
-        List<List<Cards>> playerAndGameCards = PokerLogic.getPlayerHands();
+        boolean playAnotherGame = true;
+            PokerLogic pokerLogic = new PokerLogic();
+            pokerLogic.play();
+        while (playAnotherGame) {
+            pokerLogic.setGameCards(new ArrayList<>());
+            PokerLogic.setPlayerHands(new ArrayList<>());
+            PlayerInterface.buyIn();
+            pokerLogic.dealCards();
+            PlayerInterface.roundInteraction(getNumberOfPlayers());
+            pokerLogic.dealFlop();
+            PlayerInterface.roundInteraction(getNumberOfPlayers());
+            pokerLogic.dealTurn();
+            PlayerInterface.roundInteraction(getNumberOfPlayers());
+            pokerLogic.dealRiver();
+            PlayerInterface.roundInteraction(getNumberOfPlayers());
 
-        WinCase winCase = new WinCase(allCards, playerAndGameCards, playerNames);
+            List<Cards> allCards = pokerLogic.getGameCards();
+            List<List<Cards>> playerAndGameCards = PokerLogic.getPlayerHands();
+
+            WinCase winCase = new WinCase(allCards, playerAndGameCards, playerNames);
+
+            System.out.println("Do you want to play another game? (yes/no)");
+            String playAnother = scanner.nextLine();
+            playAnotherGame = playAnother.equalsIgnoreCase("yes");
+        }
+
+        System.out.println("Thanks for playing! Goodbye.");
     }
 }
