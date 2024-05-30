b66827dfe01df29fb7ec9e7f5555b636d434aac2
callum <callumgibbons@icloud.com>
2024-05-30 09:00:28 +0100

    Minor tidy up and fixing of raising system. Also fixed error with buy in not registering a letter and crashing.

--- src/main/java/org/example/Games/Poker/PlayerInterface.java
+++ src/main/java/org/example/Games/Poker/PlayerInterface.java
@@ -1,8 +1,8 @@
 package org.example.Games.Poker;
 
-import org.example.SetUp.AllCards;
 import org.example.SetUp.Cards;
 
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -21,25 +21,35 @@ public class PlayerInterface {
     static boolean raiseOccurred = false;
 
     protected static void buyIn() {
-        Scanner buyInSelection = new Scanner(System.in); // Initialize Scanner once
+        Scanner buyInSelection = new Scanner(System.in);
         for (int i = 0; i < numberOfPlayers; i++) {
             isPlayerActive.set(i, false);
             currentPlayerChips = getPlayerChips().get(i);
             System.out.println("Player " + playerNames.get(i) + ", you have " + currentPlayerChips + " chips. Would you like to buy in for 25 chips?");
             System.out.println("1: Yes");
             System.out.println("2: No");
-            int buyInChoice = buyInSelection.nextInt();
-            if (buyInChoice == 1) {
-                currentPlayerChips -= buyInAmount;
-                playerChips.set(i, currentPlayerChips);
-                roundChips += buyInAmount;
-                System.out.println("Player " + (playerNames.get(i)) + " has bought in for 25 chips");
-                isPlayerActive.set(i, true);
-            } else if (buyInChoice == 2) {
-                System.out.println("Player " + (playerNames.get(i)) + " has not bought in for 25 chips");
-            } else {
-                System.out.println("Invalid input. Please try again.");
-                i--; // Repeat the iteration for the same player
+
+            boolean validInput = false;
+            while (!validInput) {
+                try {
+                    int buyInChoice = buyInSelection.nextInt();
+                    if (buyInChoice == 1) {
+                        currentPlayerChips -= buyInAmount;
+                        playerChips.set(i, currentPlayerChips);
+                        roundChips += buyInAmount;
+                        System.out.println("Player " + playerNames.get(i) + " has bought in for 25 chips");
+                        isPlayerActive.set(i, true);
+                        validInput = true;
+                    } else if (buyInChoice == 2) {
+                        System.out.println("Player " + playerNames.get(i) + " has not bought in for 25 chips");
+                        validInput = true;
+                    } else {
+                        System.out.println("Invalid input. Please enter 1 for Yes or 2 for No.");
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("Invalid input. Please enter 1 for Yes or 2 for No.");
+                    buyInSelection.next(); // Consume the invalid input
+                }
             }
         }
     }
@@ -48,6 +58,7 @@ public class PlayerInterface {
         Scanner playerSelection = new Scanner(System.in);
         consecutiveCallsOrChecks = 0;
         raiseOccurred = false;
+        lastBet = 0; // Reset lastBet for each round
 
         while (true) {
             int activePlayers = 0;
@@ -87,42 +98,94 @@ public class PlayerInterface {
                     displayPlayerHand(getPlayerHands().get(i));
                     System.out.println("You have " + currentPlayerChips + " chips. Please select what you would like to do:");
                     System.out.println("1: Fold");
-                    System.out.println("2: Call");
-                    if (!raiseOccurred) {
-                        System.out.println("3: Raise");
-                    }
+                    System.out.println("2: Call (" + lastBet + " chips)");
+                    System.out.println("3: Raise");
                     System.out.println("4: Check");
 
-                    playerRoundSelection = playerSelection.nextInt();
+                    try {
+                        playerRoundSelection = playerSelection.nextInt();
 
-                    if (playerRoundSelection == 1) {
-                        System.out.println("Player " + playerNames.get(i) + " has folded");
-                        correctInput = true;
-                        isPlayerActive.set(i, false);
-                        activePlayers--;
+                        if (playerRoundSelection == 1) {
+                            // Fold
+                            System.out.println("Player " + playerNames.get(i) + " has folded");
+                            correctInput = true;
+                            isPlayerActive.set(i, false);
+                            activePlayers--;
 
-                        if (activePlayers == 1) {
-                            for (int j = 0; j < numberOfPlayers; j++) {
-                                if (isPlayerActive.get(j)) {
-                                    int remainingPlayerChips = getPlayerChips().get(j) + roundChips;
-                                    getPlayerChips().set(j, remainingPlayerChips);
-                                    System.out.println("All other players have folded. " + playerNames.get(j) +
-                                            " wins the round and receives " + roundChips + " chips.");
-                                    return;// End the round
+                            if (activePlayers == 1) {
+                                for (int j = 0; j < numberOfPlayers; j++) {
+                                    if (isPlayerActive.get(j)) {
+                                        int remainingPlayerChips = getPlayerChips().get(j) + roundChips;
+                                        getPlayerChips().set(j, remainingPlayerChips);
+                                        System.out.println("All other players have folded. " + playerNames.get(j) +
+                                                " wins the round and receives " + roundChips + " chips.");
+                                        roundChips = 0; // Reset round chips after awarding to the winner
+                                        return; // End the round
+                                    }
                                 }
-                                roundChips = 0;
                             }
+                        } else if (playerRoundSelection == 2) {
+                            // Call
+                            if (currentPlayerChips >= lastBet) {
+                                currentPlayerChips -= lastBet;
+                                playerChips.set(i, currentPlayerChips);
+                                roundChips += lastBet;
+                                System.out.println("Player " + playerNames.get(i) + " calls and now has " + currentPlayerChips + " chips.");
+                                correctInput = true;
+                                consecutiveCallsOrChecks++;
+                            } else {
+                                System.out.println("Not enough chips to call. Please choose another option.");
+                            }
+                        } else if (playerRoundSelection == 3) {
+                            // Raise
+                            System.out.println("Enter raise amount (must be greater than " + lastBet + "):");
+                            int raiseAmount = playerSelection.nextInt();
+                            if (raiseAmount > lastBet && currentPlayerChips >= raiseAmount) {
+                                currentPlayerChips -= raiseAmount;
+                                playerChips.set(i, currentPlayerChips);
+                                roundChips += raiseAmount;
+                                lastBet = raiseAmount;
+                                raiseOccurred = true;
+                                consecutiveCallsOrChecks = 0; // Reset consecutiveCallsOrChecks because a raise occurred
+                                System.out.println("Player " + playerNames.get(i) + " raises by " + raiseAmount + " and now has " + currentPlayerChips + " chips.");
+                                correctInput = true;
+                            } else {
+                                System.out.println("Invalid raise amount. Please enter an amount greater than " + lastBet + " and ensure you have enough chips.");
+                            }
+                        } else if (playerRoundSelection == 4) {
+                            // Check
+                            if (lastBet == 0) {
+                                System.out.println("Player " + playerNames.get(i) + " checks.");
+                                correctInput = true;
+                                consecutiveCallsOrChecks++;
+                            } else {
+                                System.out.println("Cannot check. A bet has been made. Please choose another option.");
+                            }
+                        } else {
+                            System.out.println("Invalid input. Please try again.");
                         }
+
+                        // If all active players have called or checked consecutively, end the round
+                        if (consecutiveCallsOrChecks >= activePlayers && !raiseOccurred) {
+                            System.out.println("Round has ended.");
+                            return;
+                        }
+                    } catch (InputMismatchException e) {
+                        System.out.println("Invalid input. Please enter a number corresponding to your choice.");
+                        playerSelection.next(); // Consume the invalid input
                     }
-                    // Other actions (Call, Raise, Check) remain the same
                 }
             }
+
+            // Reset raiseOccurred at the end of the round to allow raising in the next round
+            if (consecutiveCallsOrChecks >= activePlayers) {
+                System.out.println("Round has ended.");
+                raiseOccurred = false;
+                lastBet = 0;
+                break;
+            }
         }
-
-        System.out.println("Round has ended.");
     }
-
-
 
     static void displayPlayerHand(List<Cards> playerHand) {
         for (Cards card : playerHand) {
@@ -130,5 +193,4 @@ public class PlayerInterface {
             System.out.print(" ");
         }
         System.out.println();
-    }
-}
+    }}
